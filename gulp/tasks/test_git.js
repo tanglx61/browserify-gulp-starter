966fecae04df3a96240a9d4a6002271031031c65
tanglx61 <tanglx61@gmail.com>
2014-11-23 03:14:46 -0500

    commiting test

--- gulp/tasks/test_git.js
+++ gulp/tasks/test_git.js
@@ -20,10 +20,7 @@ gulp.task('try', function(){
   gulp.src('')
     .pipe(git.add({args: '-A'}))
     .pipe(git.commit('commiting test'))
-    .pipe(git.push('origin', 'master', function (err) {
-      if (err) throw err;
-      console.log('process complete successfully');
-    }));
+   ;
 });
 
 

fca9cf8f7c427662db9c24264b9af9c7361265b7
tanglx61 <tanglx61@gmail.com>
2014-11-23 02:03:52 -0500

    'msg'

--- gulp/tasks/git.js
+++ gulp/tasks/git.js
@@ -10,22 +10,27 @@ var git = require('gulp-git');
 gulp.task('git', ['git-add', 'git-commit', 'git-push']);
 
 // Run git add with options
-gulp.task('git-add', function(){
-  	return gulp.src('')
-    	.pipe(git.add({args: '-A'}));
+gulp.task('git-add', function(callback){
+	gulp.src('./')
+		.pipe(git.add({args: '-A'}));
+    
+    callback();
 });
 
 
 
-gulp.task('git-commit', function(){
-	return gulp.src('')
-		.pipe(git.commit('testing gulp git flow' ));
+gulp.task('git-commit', function(callback){
+	gulp.src('./')
+		.pipe(git.commit('testing gulp git flow again' ));
+	
+	callback();
 });
 
 
-gulp.task('git-push', function(){
+gulp.task('git-push', function(callback){
   git.push('origin', 'master', function (err) {
     if (err) throw err;
+    else callback();
   });
 });
 

48294db1b1f385a8b4bba0eaf3dece48c228d00b
tanglx61 <tanglx61@gmail.com>
2014-11-23 03:37:57 -0500

    dist: 2014-11-23-03-37-57

--- gulp/tasks/git.js
+++ gulp/tasks/git.js
@@ -4,62 +4,27 @@
 */
 var gulp = require('gulp');
 var git = require('gulp-git');
+var moment = require('moment');
 
-//aaaa
 
-gulp.task('git', function(callback){
-	/*gulp.src(['./package.json'])
-		.pipe(git.add({args: '-A'}))
-		.pipe(git.commit('testing commit'))
-		.pipe(git.push('origin', 'master', function(err){
-			if (err) throw err;
-			callback();
-		}));*/
-	git.add({args: '-A'});
-	git.commit('testing commit msg');
-	
+gulp.task('git', function(){
+  gulp.src('./')
+    .pipe(git.add({args: '-A'}))
+    .pipe(git.commit(getCommitMessage(), {}, push));
+   
+});
 
-	console.log('git push');
-	git.push('origin', 'master', function(err){
-		if (err) throw err;
-		console.log('done');
-		callback();
+
+function push(){
+	git.push('origin', 'master', function (err) {
+	  if (err) throw err;
+	  console.log('github push complete!');
 	});
-});
+}
 
-gulp.task('testgit', ['git-add', 'git-commit']);
-
-// Run git add with options
-gulp.task('git-add', function(callback){
-	gulp.src('./package.json')
-		.pipe(git.add({args: '-A'}));
-    console.log('git added');
-    callback();
-});
-
-
-
-gulp.task('git-commit', function(callback){
-	gulp.src('./')
-		.pipe(git.commit('git test' ));
-	console.log('git commited');
-	callback();
-});
-
-
-gulp.task('git-push', function(callback){
-  git.push('origin', 'master', function (err) {
-    if (err) throw err;
-    else callback();
-  });
-});
-
-
-gulp.task('git-pull', function(){
-  git.pull('origin', 'master', function (err) {
-    if (err) throw err;
-  });
-});
+function getCommitMessage(){
+	return 'dist: ' + moment().format('YYYY-MM-DD-hh-mm-ss');
+}
 
 
 
